5aae7a52380eaacd7d71b833e5acc670eb264973
KetAveryH <khollingsworth@g.hmc.edu>
2024-09-23 02:50:29 -0700

    System Completely working, need to assign pins to second set of switches

--- fpga/lab2/source/lab2_kh/top.sv
+++ fpga/lab2/source/lab2_kh/top.sv
@@ -3,100 +3,135 @@
 // 9/18/2023
 // Summary: TODO
 
-module Hz_blink(input logic reset, output logic [1:0] switch);
-	logic int_osc;
+module Hz_blink(input logic reset, output logic [1:0] display_select_c, output logic int_osc);
+	//logic int_osc;
 	logic [24:0] counter;
-	logic first;
+	logic switch;
+	logic [1:0] display_select;
 	
 	// Internal high-speed oscillator
     HSOSC #(.CLKHF_DIV(2'b01)) 
           hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 	
 	always_ff @(posedge int_osc) begin
-	  if(reset == 0 || !first) begin
-		  switch <= 2'b01;
+	  if(reset == 0) begin
+		  switch <= 0;
 		  counter <= 0;
-		  first <= 1;
 		 end
 		 
-	  if(counter >= 10_000_000) begin 
+	  if(counter >= 150_000) begin 
 		      counter <= 0;
 		      switch <= ~switch;
-	  end
-	  
-	  else    counter <= counter + 25'd1;
+	  end else    
+		  counter <= counter + 25'd1;
+		  
+	  if (switch == 0) 
+		  display_select <= 2'b01;
+	  else 
+		  display_select <= 2'b10;
 	end
+	
+	assign display_select_c = display_select;
+	
 endmodule
 
-module led_comb(input logic [3:0] s,
-			   output logic [1:0] led);
+module led_comb(input logic [3:0] s1,
+				input logic [3:0] s2,
+			   output logic [4:0] led);
 
-	always_comb
-	  case(s[1:0])
-		  2'd0: led[0] = 0;
-		  2'd1: led[0] = 1;
-		  2'd2: led[0] = 1;
-		  2'd3: led[0] = 0;
-	endcase
+	logic [4:0] s3;
 	
-	always_comb
-	  case(s[3:2])
-		  2'd0: led[1] = 0;
-		  2'd1: led[1] = 0;
-		  2'd2: led[1] = 0;
-		  2'd3: led[1] = 1;
-	endcase
+	always_comb begin
+		s3 = s1 + s2;
+	end
+
+	assign led = s3;
+
+	//always_comb
+	  //case(s1[1:0])
+		  //2'd0: led[0] = 0;
+		  //2'd1: led[0] = 1;
+		  //2'd2: led[0] = 1;
+		  //2'd3: led[0] = 0;
+	//endcase
+	
+	//always_comb
+	  //case(s1[3:2])
+		  //2'd0: led[1] = 0;
+		  //2'd1: led[1] = 0;
+		  //2'd2: led[1] = 0;
+		  //2'd3: led[1] = 1;
+	//endcase
 			   
 endmodule
 
-module seven_seg_display(input logic [3:0] s,
-						 //input logic [1:0] switch,
+module seven_seg_display(input logic  [3:0] s1,
+						 input logic  [3:0] s2,
+						 input logic  [1:0] display_select_c,
+						 input logic       int_osc,
 						 output logic [6:0] seg); 
+	logic [3:0] s_select;
+	
+	always_ff @(posedge int_osc) begin
+		if (display_select_c[1]) 
+			s_select <= s1;
+		else
+			s_select <= s2;
+	end
+		
 	always_comb
-	  case(s)
+	  case(s_select)
 		  //		 	     abc defg
-		4'h0:		seg = 7'b000_0001;
-		4'h1:		seg = 7'b100_1111;
-		4'h2:		seg = 7'b001_0010;
-		4'h3:		seg = 7'b000_0110;
-		4'h4:		seg = 7'b100_1100;
-		4'h5:		seg = 7'b010_0100;
-		4'h6:		seg = 7'b010_0000;
-		4'h7:		seg = 7'b000_1111;
-		4'h8:		seg = 7'b000_0000;
-		4'h9:		seg = 7'b000_1100;
-		4'hA:		seg = 7'b000_1000;
-		4'hB:		seg = 7'b110_0000;
-		4'hC:		seg = 7'b011_0001;
-		4'hD:		seg = 7'b100_0010;
-		4'hE:		seg = 7'b011_0000;
-		4'hF:		seg = 7'b011_1000;
+		4'h00:		seg = 7'b000_0001;
+		4'h01:		seg = 7'b100_1111;
+		4'h02:		seg = 7'b001_0010;
+		4'h03:		seg = 7'b000_0110;
+		4'h04:		seg = 7'b100_1100;
+		4'h05:		seg = 7'b010_0100;
+		4'h06:		seg = 7'b010_0000;
+		4'h07:		seg = 7'b000_1111;
+		4'h08:		seg = 7'b000_0000;
+		4'h09:		seg = 7'b000_1100;
+		4'h0A:		seg = 7'b000_1000;
+		4'h0B:		seg = 7'b110_0000;
+		4'h0C:		seg = 7'b011_0001;
+		4'h0D:		seg = 7'b100_0010;
+		4'h0E:		seg = 7'b011_0000;
+		4'h0F:		seg = 7'b011_1000;
 		default: 	seg = 7'b111_1111;
+		
 	endcase
 endmodule
 
-module lab1_kh(input logic reset,
-			   input logic [3:0] s,
-			   output logic [2:0] led,
-			   output logic [1:0] switch,
+module lab2_kh(input logic reset,
+			   input  logic [3:0] s1,
+			   //input  logic [3:0] s2,
+			   output logic [3:0] led,
+			   output logic [1:0] display_select_c,
 			   output logic [6:0] seg);
+	logic int_osc;
+	logic [3:0] s2 = 4'b0001;
 
+	
 	
 	Hz_blink u1 (
 		.reset(reset),
-		.switch(switch[1:0]) // switch alternates between 01 and 10
+		.display_select_c(display_select_c), // switch alternates between 01 and 10
+		.int_osc(int_osc)
 	);
 	
 	led_comb u2 (
-		.s(s),
-		.led(led[1:0])
+		.s1(s1),
+		.s2(s2),
+		.led(led[3:0])
 	);
 	
 	seven_seg_display u3 (
-		.s(s),
-		.seg(seg)
-		//,
-		//.switch(switch)
+		.s1(s1),
+		.s2(s2),
+		.display_select_c(display_select_c),
+		.seg(seg),
+		.int_osc(int_osc)
 	);
 	
 	
